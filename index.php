cbd30405d31995a9130fd5e7a8f2ca1be20aaf15
lizardcat <alex.raza.ke@gmail.com>
2025-06-10 09:29:56 +0300

    Saving text

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <h1> Welcome to Bob's Auto Parts!</h1>
 
-<p>WHat would you like to order today?</p>
+<p>What would you like to order today?</p>
 
 <?php 
     echo "<p> Order processed at ".date('H:i, jS F Y')."</p";
