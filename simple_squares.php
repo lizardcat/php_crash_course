21eb7c36ac347b76e65eaae893a3bab2cb91c0df
lizardcat <alex.raza.ke@gmail.com>
2025-07-09 16:57:37 +0300

    Simplifying square exercise

--- simple_squares.php
+++ simple_squares.php
@@ -1,7 +1,6 @@
 <?php 
 
-$num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-for ($i = 0; $i <= 9; $i++) {
-	$square = $num[$i] * $num[$i];
-	echo $square . " ";
+
+for ($i = 1; $i <= 10; $i++) {
+	echo ($i * $i) . " ";
 }
